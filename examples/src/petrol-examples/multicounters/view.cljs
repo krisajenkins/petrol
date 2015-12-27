14fd85f483fa4f3819cce125e4287a0d9fa53ed6
Kris Jenkins <krisajenkins@gmail.com>
2015-12-27 23:39:03 +0000

    Tidying up @jellea's multicounters example.

--- examples/src/petrol-examples/multicounters/view.cljs
+++ examples/src/petrol-examples/multicounters/view.cljs
@@ -3,18 +3,24 @@
             [petrol-examples.multicounters.messages :as m]
             [petrol-examples.counter2.view :as counter2]))
 
+(defn counter-container
+  [ui-channel index counter]
+  [:div.col-xs-12.col-sm-4 {:key index}
+   [counter2/root (forward (partial m/->UpdateCounter index)
+                           ui-channel)
+    counter]
+   [:button.btn.btn-info {:on-click (send! ui-channel (m/->DelCounter index))}
+    "Delete counter"]])
+
 (defn root
   [ui-channel app]
   [:div.container
    [:h1 "Multiple Counters: " (count (:counters app))]
 
    [:div
-    [:button {:on-click (send! ui-channel (m/->AddCounter))} "Add counter"]]
+    [:button.btn.btn-info {:on-click (send! ui-channel (m/->AddCounter))}
+     "Add counter"]]
 
    [:div.row
-    [:div.col-xs-12.col-sm-6
-     (for [[key val] (:counters app)] ^{:key key}
-      [:div
-       [counter2/root (forward (partial m/->UpdateCounter key)
-                               ui-channel) val]
-       [:button {:on-click (send! ui-channel (m/->DelCounter key))} "Delete counter"]])]]])
+    (map-indexed (partial counter-container ui-channel)
+                 (:counters app))]])
